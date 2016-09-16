94ba907da3b2c98fb1c46b170f0d9a5e1603f490
Denis O <denis@universal-development.com>
2016-09-16 19:25:47 +0300

    porting template app project

---
+++ app/src/main/java/com/unidev/ProjectnameApplication.java
@@ -0,0 +1,12 @@
+package com.unidev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectnameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectnameApplication.class, args);
+	}
+}
---
+++ app/src/test/java/com/unidev/ProjectnameApplicationTests.java
@@ -0,0 +1,16 @@
+package com.unidev;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectnameApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
