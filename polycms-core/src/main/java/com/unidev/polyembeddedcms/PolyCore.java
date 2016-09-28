df4d756eaf0ab630d37bd90d2eab4d8621d4416e
Denis <denis@universal-development.com>
2016-09-28 10:37:00 +0300

    Poly tenant save method

--- polycms-core/src/main/java/com/unidev/polyembeddedcms/PolyCore.java
+++ polycms-core/src/main/java/com/unidev/polyembeddedcms/PolyCore.java
@@ -1,7 +1,43 @@
 package com.unidev.polyembeddedcms;
 
+import com.unidev.polydata.FlatFileStorage;
+import com.unidev.polydata.FlatFileStorageMapper;
+import com.unidev.polydata.domain.BasicPoly;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+
 /**
- *
+ * Service for poly tenants management
  */
+@Service
 public class PolyCore {
+    public static final String INDEX_FILE = "index.json";
+    private String storageRoot;
+
+    public File fetchStorageRoot(String tenantName) {
+        File storageFile = new File(storageRoot + "/" + tenantName);
+        return storageFile;
+    }
+
+    public void createTenantSorage(String tenantName) {
+        File tenantIndexFile = new File(storageRoot + "/" + INDEX_FILE);
+
+        FlatFileStorage tenantIndex =
+                FlatFileStorageMapper.storageMapper().loadSource(tenantIndexFile).load();
+
+        tenantIndex.add(new BasicPoly()._id(tenantName));
+
+        FlatFileStorageMapper.storageMapper().saveSource(tenantIndexFile).save(tenantIndex);
+    }
+
+    // ========================
+
+    public String getStorageRoot() {
+        return storageRoot;
+    }
+
+    public void setStorageRoot(String storageRoot) {
+        this.storageRoot = storageRoot;
+    }
 }
