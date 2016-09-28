5598a2e2119ad52d675c4160575b0ba53e88d543
Denis <denis@universal-development.com>
2016-09-28 09:47:46 +0300

    Added poly exception

---
+++ polycms-core/src/main/java/com/unidev/polyembeddedcms/PolyCoreException.java
@@ -0,0 +1,9 @@
+package com.unidev.polyembeddedcms;
+
+import com.unidev.platform.common.exception.CommonRuntimeException;
+
+/**
+ * Exception thrown on poly core error
+ */
+public class PolyCoreException extends CommonRuntimeException {
+}
