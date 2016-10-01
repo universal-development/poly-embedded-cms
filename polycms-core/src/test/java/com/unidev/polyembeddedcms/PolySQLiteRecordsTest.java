80526c9a8effcf3c3f5e2dd4f4b9a3e9d0bc225a
Denis O <denis@universal-development.com>
2016-10-01 12:36:29 +0300

    Test for sqlite operations

---
+++ polycms-core/src/test/java/com/unidev/polyembeddedcms/PolySQLiteRecordsTest.java
@@ -0,0 +1,66 @@
+package com.unidev.polyembeddedcms;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+
+import com.unidev.polydata.SQLiteStorage;
+import com.unidev.polydata.SQLiteStorageException;
+import com.unidev.polydata.domain.BasicPoly;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * SQLite poly records generation tests
+ */
+public class PolySQLiteRecordsTest {
+
+    private PolyCore polyCore;
+
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+    @Before
+    public void setup() {
+        polyCore = new PolyCore();
+        polyCore.setStorageRoot(temporaryFolder.getRoot().getAbsolutePath());
+        polyCore.createIfNotExistTenantIndexFile();
+    }
+
+    @Test
+    public void testContentGeneration() throws SQLiteStorageException, SQLException {
+        String tenant = "testTenant";
+
+        polyCore.createTenantStorage(tenant);
+
+        File root = polyCore.fetchStorageRoot(tenant);
+
+        File db = new File(root, "poly.db");
+
+        SQLiteStorage sqLiteStorage = new SQLiteStorage(db.getPath());
+        sqLiteStorage.setPolyMigrators(PolySQLiteMigrator.sqLitePolyMigratorList());
+
+
+        BasicPoly record = new BasicPoly()._id("tomato");
+        record.put("label", "Tomato");
+        record.put("data", new BasicPoly());
+
+        sqLiteStorage.save("tomato", record);
+
+        Connection connection = sqLiteStorage.openDb();
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM tomato;");
+
+        List<BasicPoly> polyList = sqLiteStorage.evaluateStatement(preparedStatement);
+
+        assertThat(polyList.size(), is(1));
+    }
+
+}
