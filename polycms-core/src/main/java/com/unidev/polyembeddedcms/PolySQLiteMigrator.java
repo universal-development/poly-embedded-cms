75f2ec607c6a5ebb24b3b1d90ca44c938b3aca4c
Denis O <denis@universal-development.com>
2016-10-01 11:21:49 +0300

    Added basic poly migrator, which create basic json

---
+++ polycms-core/src/main/java/com/unidev/polyembeddedcms/PolySQLiteMigrator.java
@@ -0,0 +1,37 @@
+package com.unidev.polyembeddedcms;
+
+import com.unidev.polydata.SQLitePolyMigrator;
+import com.unidev.polydata.SQLiteStorageException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Default SQLite poly migrator, create poly table with (_id, label, json)
+ *
+ */
+public class PolySQLiteMigrator implements SQLitePolyMigrator {
+
+    private static Logger LOG = LoggerFactory.getLogger(PolySQLiteMigrator.class);
+
+
+    @Override
+    public boolean canHandle(String poly) {
+        LOG.info("Default handling for any poly {}", poly);
+        return true;
+    }
+
+    @Override
+    public void handle(String poly, Connection connection) throws SQLiteStorageException {
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS "+poly+" (_id VARCHAR(255) PRIMARY KEY, label VARCHAR(255), value JSON)");
+        } catch (SQLException e) {
+            LOG.warn("Failed to run migration", e);
+            throw new SQLiteStorageException(e);
+        }
+    }
+}
