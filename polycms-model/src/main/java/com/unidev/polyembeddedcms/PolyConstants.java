0163e55c5a648fb259b352b14b62ea6181e8ca0c
Denis <denis@universal-development.com>
2016-10-03 13:57:35 +0300

    Added poly constants

---
+++ polycms-model/src/main/java/com/unidev/polyembeddedcms/PolyConstants.java
@@ -0,0 +1,12 @@
+package com.unidev.polyembeddedcms;
+
+/**
+ * Constant keys used in poly records
+ */
+public class PolyConstants {
+    public static final String ID_KEY = "_id";
+    public static final String DATE_KEY = "date";
+    public static final String CATEGORY_KEY = "category";
+    public static final String TAGS_KEY = "tags";
+    public static final String DATA_KEY = "data";
+}
