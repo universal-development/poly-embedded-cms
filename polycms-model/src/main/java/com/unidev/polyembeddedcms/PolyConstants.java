1d3b2a23d5b951eb641d379f89d918f2577e5a5f
Denis <denis@universal-development.com>
2016-10-03 14:33:39 +0300

    ITEM_PER_PAGE_KEY

--- polycms-model/src/main/java/com/unidev/polyembeddedcms/PolyConstants.java
+++ polycms-model/src/main/java/com/unidev/polyembeddedcms/PolyConstants.java
@@ -13,6 +13,8 @@ public class PolyConstants {
     public static final String LABEL_KEY = "label";
     public static final String COUNT_KEY = "count";
 
+    public static final String ITEM_PER_PAGE_KEY = "item_per_page";
+
 
     public static final String CATEGORY_POLY = "category";
     public static final String TAGS_POLY = "tag";
