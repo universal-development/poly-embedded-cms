44f406341aeb44d5b2c625d6a406c22f6801ef5b
Denis <denis@universal-development.com>
2016-10-04 17:51:14 +0300

    Added listing of tags

--- polycms-app-web/src/test/java/com/unidev/ProjectnameApplicationTests.java
+++ polycms-app-web/src/test/java/com/unidev/ProjectnameApplicationTests.java
@@ -1,16 +1,79 @@
 package com.unidev;
 
+import com.unidev.polycms.web.WebPolyCore;
+import com.unidev.polydata.SQLiteStorage;
+import com.unidev.polydata.SQLiteStorageException;
+import com.unidev.polydata.domain.BasicPoly;
+import com.unidev.polyembeddedcms.PolyConstants;
+import com.unidev.polyembeddedcms.PolyCore;
+import com.unidev.polyembeddedcms.PolyRecord;
+import com.unidev.polyembeddedcms.TagsPolyMigrator;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.Connection;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProjectnameApplicationTests {
 
+	@Autowired
+	private WebPolyCore webPolyCore;
+
+	@Autowired
+	private PolyCore polyCore;
+
+
 	@Test
 	public void contextLoads() {
+
+
+	}
+
+	@Test
+	public void testTagsListing() throws MalformedURLException, SQLiteStorageException {
+		String domain = "test.com";
+		File root = polyCore.fetchStorageRoot(domain);
+		root.mkdirs();
+
+		File dbFile = new File(root, PolyConstants.DB_FILE);
+		dbFile.deleteOnExit();
+		SQLiteStorage sqLiteStorage = new SQLiteStorage(dbFile.getAbsolutePath());
+		sqLiteStorage.setPolyMigrators(Arrays.asList(new TagsPolyMigrator()));
+
+		PolyRecord tagTomato = new PolyRecord()._id("tomato").label("Tomato").count(100);
+		sqLiteStorage.save(PolyConstants.TAGS_POLY, tagTomato);
+
+		PolyRecord tagPotato = new PolyRecord()._id("potato").label("Potato").count(10);
+		sqLiteStorage.save(PolyConstants.TAGS_POLY, tagPotato);
+
+
+		HttpServletRequest httpServletRequest = Mockito.mock(HttpServletRequest.class);
+		Mockito.doReturn(new StringBuffer("http://" + domain)).when(httpServletRequest).getRequestURL();
+
+		List<BasicPoly> list = webPolyCore.fetchTags(httpServletRequest);
+		assertThat(list, is(not(nullValue())));
+		assertThat(list.size(), is(2));
+
+		BasicPoly first = list.get(0);
+
+		assertThat(first.get(PolyConstants.COUNT_KEY), is(100));
+		assertThat(first._id(), is("tomato"));
+
+
 	}
 
 }
