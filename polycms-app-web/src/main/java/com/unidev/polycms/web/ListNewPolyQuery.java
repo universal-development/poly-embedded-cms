619d4cbac27e0f0797645c977009ef0ea4dd3b16
Denis O <denis@universal-development.com>
2016-10-10 20:11:20 +0300

    Added query object

---
+++ polycms-app-web/src/main/java/com/unidev/polycms/web/ListNewPolyQuery.java
@@ -0,0 +1,64 @@
+package com.unidev.polycms.web;
+
+/**
+ * Query object for new new polys
+ */
+public class ListNewPolyQuery {
+
+    private String tag;
+    private String category;
+    private Long page;
+
+    public ListNewPolyQuery() {}
+
+    public ListNewPolyQuery(String tag, String category, Long page) {
+        this.tag = tag;
+        this.category = category;
+        this.page = page;
+    }
+
+    public static ListNewPolyQuery query() {
+        return new ListNewPolyQuery();
+    }
+
+    public ListNewPolyQuery withTag(String tag) {
+        setTag(tag);
+        return this;
+    }
+
+    public ListNewPolyQuery withCategory(String category) {
+        setCategory(category);
+        return this;
+    }
+
+
+    public ListNewPolyQuery withPage(Long page) {
+        setPage(page);
+        return this;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Long getPage() {
+        return page;
+    }
+
+    public void setPage(Long page) {
+        this.page = page;
+    }
+
+}
