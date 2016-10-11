16ae3db02eb80a1ca212ff3c0c32fc2385436f21
Denis <denis@universal-development.com>
2016-10-11 14:48:06 +0300

    Imports cleanup

--- polycms-app-hateoas/src/main/java/com/unidev/polycms/StorageController.java
+++ polycms-app-hateoas/src/main/java/com/unidev/polycms/StorageController.java
@@ -1,8 +1,6 @@
 package com.unidev.polycms;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
