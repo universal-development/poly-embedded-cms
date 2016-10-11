d4388ee4c8dfad96a63aa6c6ab4dd4f4d61ed792
Denis <denis@universal-development.com>
2016-10-11 11:09:39 +0300

    Usage of sqlite service in web interface

--- polycms-app-web/src/main/java/com/unidev/polycms/web/WebPolyCore.java
+++ polycms-app-web/src/main/java/com/unidev/polycms/web/WebPolyCore.java
@@ -1,26 +1,17 @@
 package com.unidev.polycms.web;
 
-import com.unidev.platform.common.utils.StringUtils;
 import com.unidev.platform.j2ee.common.WebUtils;
-import com.unidev.polydata.SQLiteStorage;
 import com.unidev.polydata.domain.BasicPoly;
-import com.unidev.polyembeddedcms.PolyConstants;
-import com.unidev.polyembeddedcms.PolyCore;
-import javax.servlet.http.HttpServletRequest;
-
 import com.unidev.polyembeddedcms.PolyQuery;
 import com.unidev.polyembeddedcms.PolyRecord;
+import com.unidev.polyembeddedcms.SQLitePolyService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.util.HashMap;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Core service for web app
@@ -30,13 +21,14 @@ public class WebPolyCore {
 
     private static Logger LOG = LoggerFactory.getLogger(WebPolyCore.class);
 
-    private PolyCore polyCore;
+
+    private SQLitePolyService sqLitePolyService;
 
     private WebUtils webUtils;
 
 
-    public WebPolyCore( @Autowired PolyCore polyCore, @Autowired WebUtils webUtils) {
-        this.polyCore = polyCore;
+    public WebPolyCore( @Autowired SQLitePolyService sqLitePolyService, @Autowired WebUtils webUtils) {
+        this.sqLitePolyService = sqLitePolyService;
         this.webUtils = webUtils;
     }
 
@@ -46,40 +38,8 @@ public class WebPolyCore {
      * @return
      */
     public List<BasicPoly> listNewPoly(PolyQuery listNewPolyQuery, HttpServletRequest httpServletRequest) {
-        SQLiteStorage sqLiteStorage = fetchSqliteDB(httpServletRequest);
-        PreparedStatement preparedStatement;
-        try(Connection connection = sqLiteStorage.openDb()) {
-            Integer id = 1;
-            Map<Integer, Object> params = new HashMap<>();
-
-            StringBuilder query = new StringBuilder("SELECT * FROM " + PolyConstants.DATA_POLY + " WHERE 1=1 ");
-
-            if (!StringUtils.isBlank(listNewPolyQuery.getCategory())) {
-                query.append(  " AND " + PolyConstants.CATEGORY_KEY + " = ?");
-                params.put(id++, listNewPolyQuery.getCategory() );
-            }
-
-            if (!StringUtils.isBlank(listNewPolyQuery.getTag())) {
-                query.append(  " AND " + PolyConstants.TAGS_KEY + " LIKE ?");
-                params.put(id++, listNewPolyQuery.getTag() );
-            }
-
-            query.append(" ORDER BY date DESC LIMIT ? OFFSET ?");
-            params.put(id++, listNewPolyQuery.getItemPerPage());
-            params.put(id++, listNewPolyQuery.getItemPerPage() * listNewPolyQuery.getPage());
-
-            preparedStatement = connection.prepareStatement(query.toString());
-            for(Map.Entry<Integer, Object> entry : params.entrySet()) {
-                preparedStatement.setObject(entry.getKey(), entry.getValue());
-            }
-
-            List<BasicPoly> polyList = sqLiteStorage.evaluateStatement(preparedStatement);
-            return polyList;
-        } catch (Exception e) {
-            e.printStackTrace();
-            LOG.warn("Failed to fetch categories for request {}", httpServletRequest);
-            return null;
-        }
+        String tenant = fetchTenant(httpServletRequest);
+        return sqLitePolyService.listNewPoly(listNewPolyQuery, tenant);
     }
 
     /**
@@ -88,19 +48,8 @@ public class WebPolyCore {
      * @return
      */
     public List<BasicPoly> fetchCategories(HttpServletRequest httpServletRequest) {
-        SQLiteStorage sqLiteStorage = fetchSqliteDB(httpServletRequest);
-
-        PreparedStatement preparedStatement;
-        try(Connection connection = sqLiteStorage.openDb()) {
-            preparedStatement = connection.prepareStatement("SELECT * FROM " + PolyConstants.CATEGORY_POLY);
-            List<BasicPoly> polyList = sqLiteStorage.evaluateStatement(preparedStatement);
-            return polyList;
-        } catch (Exception e) {
-            e.printStackTrace();
-            LOG.warn("Failed to fetch categories for request {}", httpServletRequest);
-            return null;
-        }
-
+        String tenant = fetchTenant(httpServletRequest);
+        return sqLitePolyService.fetchCategories(tenant);
     }
 
     /**
@@ -109,19 +58,8 @@ public class WebPolyCore {
      * @return
      */
     public List<BasicPoly> fetchTags(HttpServletRequest httpServletRequest) {
-        SQLiteStorage sqLiteStorage = fetchSqliteDB(httpServletRequest);
-
-        PreparedStatement preparedStatement;
-        try(Connection connection = sqLiteStorage.openDb()) {
-            preparedStatement = connection.prepareStatement("SELECT * FROM " + PolyConstants.TAGS_POLY + " ORDER BY count DESC");
-            List<BasicPoly> polyList = sqLiteStorage.evaluateStatement(preparedStatement);
-            return polyList;
-        } catch (Exception e) {
-            e.printStackTrace();
-            LOG.warn("Failed to fetch tags for request {}", httpServletRequest);
-            return null;
-        }
-
+        String tenant = fetchTenant(httpServletRequest);
+        return sqLitePolyService.fetchTags(tenant);
     }
 
     /**
@@ -131,22 +69,8 @@ public class WebPolyCore {
      * @return poly instance or null if poly is not available
      */
     public PolyRecord fetchPoly(String id, HttpServletRequest httpServletRequest) {
-        SQLiteStorage sqLiteStorage = fetchSqliteDB(httpServletRequest);
-
-        PreparedStatement preparedStatement;
-        try(Connection connection = sqLiteStorage.openDb()) {
-            preparedStatement = connection.prepareStatement("SELECT * FROM " + PolyConstants.DATA_POLY + " WHERE _id = ?");
-            preparedStatement.setObject(1, id);
-            List<BasicPoly> polyList = sqLiteStorage.evaluateStatement(preparedStatement);
-            if (polyList.size() != 1) {
-                return null;
-            }
-            return new PolyRecord(polyList.get(0));
-        } catch (Exception e) {
-            e.printStackTrace();
-            LOG.warn("Failed to fetch poly {} for request {}", id, httpServletRequest);
-            return null;
-        }
+        String tenant = fetchTenant(httpServletRequest);
+        return sqLitePolyService.fetchPoly(id, tenant);
     }
 
     /**
@@ -154,20 +78,11 @@ public class WebPolyCore {
      * @param httpServletRequest
      * @return
      */
-    public File fetchTenantRoot(HttpServletRequest httpServletRequest) {
+    public String fetchTenant(HttpServletRequest httpServletRequest) {
         String domain = webUtils.getDomain(httpServletRequest);
-        return polyCore.fetchStorageRoot(domain);
+        return domain;
     }
 
 
-    public boolean existsTenant(HttpServletRequest httpServletRequest) {
-        return fetchTenantRoot(httpServletRequest).exists();
-    }
-
-    private SQLiteStorage fetchSqliteDB(HttpServletRequest httpServletRequest) {
-        File tenantRoot = fetchTenantRoot(httpServletRequest);
-        File dbFile = new File(tenantRoot, PolyConstants.DB_FILE);
-        return new SQLiteStorage(dbFile.getAbsolutePath());
-    }
 
 }
