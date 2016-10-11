f2407ba670fd66a9d38cfb4f83e45c9ebe10bb1d
Denis <denis@universal-development.com>
2016-10-11 17:12:31 +0300

    Usage of WebPolyQuery in poly core

--- polycms-app-web/src/main/java/com/unidev/polycms/web/WebPolyCore.java
+++ polycms-app-web/src/main/java/com/unidev/polycms/web/WebPolyCore.java
@@ -2,6 +2,7 @@ package com.unidev.polycms.web;
 
 import com.unidev.platform.j2ee.common.WebUtils;
 import com.unidev.polydata.domain.BasicPoly;
+import com.unidev.polyembeddedcms.PolyCore;
 import com.unidev.polyembeddedcms.PolyQuery;
 import com.unidev.polyembeddedcms.PolyRecord;
 import com.unidev.polyembeddedcms.SQLitePolyService;
@@ -11,25 +12,89 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Core service for web app
  */
 @Service
 public class WebPolyCore {
-
+    public static final int ITEM_PER_PAGE = 40;
     private static Logger LOG = LoggerFactory.getLogger(WebPolyCore.class);
-
 
     private SQLitePolyService sqLitePolyService;
 
     private WebUtils webUtils;
 
+    private PolyCore polyCore;
 
-    public WebPolyCore( @Autowired SQLitePolyService sqLitePolyService, @Autowired WebUtils webUtils) {
+
+    public WebPolyCore addCategories(WebPolyQuery polyRequest) {
+        List<BasicPoly> categories = fetchCategories(polyRequest.request());
+        polyRequest.model().addAttribute("categories", categories);
+        return this;
+    }
+
+    public WebPolyCore addTags(WebPolyQuery polyRequest) {
+        List<BasicPoly> tags = fetchCategories(polyRequest.request());
+        polyRequest.model().addAttribute("tags", tags);
+        return this;
+    }
+
+    /**
+     * Add support objects to model
+     * @param polyRequest
+     * @return
+     */
+    public WebPolyCore addSupportModel(WebPolyQuery polyRequest) {
+        return addCategories(polyRequest).addTags(polyRequest);
+    }
+
+    public WebPolyCore addNew(WebPolyQuery polyRequest, String urlBegin) {
+        List<BasicPoly> polyList = listNewPoly(polyRequest, polyRequest.request());
+        return addPagination(polyRequest, urlBegin, polyList);
+    }
+
+    public WebPolyCore addPagination(WebPolyQuery polyRequest, String urlBegin, List<BasicPoly> items) {
+        polyRequest.model().addAttribute("page", polyRequest.page());
+        polyRequest.model().addAttribute("items", items);
+
+        Long backPage = polyRequest.page() - 1;
+        Long nextPage = polyRequest.page() + 1;
+
+        if (backPage <= 0) {
+            polyRequest.model().addAttribute("backPage", null);
+        } else {
+            polyRequest.model().addAttribute("backPage", urlBegin + backPage);
+        }
+
+        if (items != null && items.size() > 0) {
+            polyRequest.model().addAttribute("nextPage", urlBegin + nextPage);
+        } else {
+            polyRequest.model().addAttribute("nextPage", null);
+        }
+
+        List<Map<String, String>> pages = new ArrayList<>();
+        for(Long pageId = polyRequest.page() - WebPolyCore.ITEM_PER_PAGE / 2;pageId<= polyRequest.page() + WebPolyCore.ITEM_PER_PAGE / 2;pageId++) {
+            if (pageId >= 1) {
+                Map<String, String> pageData = new HashMap<>();
+                pageData.put("page",urlBegin + pageId);
+                pageData.put("pageId", pageId + "");
+                pages.add(pageData);
+            }
+        }
+        polyRequest.model().addAttribute("pages", pages);
+        return this;
+    }
+
+
+    public WebPolyCore( @Autowired SQLitePolyService sqLitePolyService, @Autowired WebUtils webUtils, @Autowired PolyCore polyCore) {
         this.sqLitePolyService = sqLitePolyService;
         this.webUtils = webUtils;
+        this.polyCore = polyCore;
     }
 
     /**
@@ -37,7 +102,7 @@ public class WebPolyCore {
      * @param httpServletRequest
      * @return
      */
-    public List<BasicPoly> listNewPoly(PolyQuery listNewPolyQuery, HttpServletRequest httpServletRequest) {
+    private List<BasicPoly> listNewPoly(PolyQuery listNewPolyQuery, HttpServletRequest httpServletRequest) {
         String tenant = fetchTenant(httpServletRequest);
         return sqLitePolyService.listNewPoly(listNewPolyQuery, tenant);
     }
@@ -47,7 +112,7 @@ public class WebPolyCore {
      * @param httpServletRequest
      * @return
      */
-    public List<BasicPoly> fetchCategories(HttpServletRequest httpServletRequest) {
+    private List<BasicPoly> fetchCategories(HttpServletRequest httpServletRequest) {
         String tenant = fetchTenant(httpServletRequest);
         return sqLitePolyService.fetchCategories(tenant);
     }
@@ -57,7 +122,7 @@ public class WebPolyCore {
      * @param httpServletRequest
      * @return
      */
-    public List<BasicPoly> fetchTags(HttpServletRequest httpServletRequest) {
+    private List<BasicPoly> fetchTags(HttpServletRequest httpServletRequest) {
         String tenant = fetchTenant(httpServletRequest);
         return sqLitePolyService.fetchTags(tenant);
     }
@@ -68,7 +133,7 @@ public class WebPolyCore {
      * @param httpServletRequest
      * @return poly instance or null if poly is not available
      */
-    public PolyRecord fetchPoly(String id, HttpServletRequest httpServletRequest) {
+    private PolyRecord fetchPoly(String id, HttpServletRequest httpServletRequest) {
         String tenant = fetchTenant(httpServletRequest);
         return sqLitePolyService.fetchPoly(id, tenant);
     }
@@ -78,7 +143,7 @@ public class WebPolyCore {
      * @param httpServletRequest
      * @return
      */
-    public String fetchTenant(HttpServletRequest httpServletRequest) {
+    private String fetchTenant(HttpServletRequest httpServletRequest) {
         String domain = webUtils.getDomain(httpServletRequest);
         return domain;
     }
