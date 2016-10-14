4f1b167148bdf7166a099bdb4b6717029feb5ad5
Denis O <denis@universal-development.com>
2016-10-14 11:54:45 +0300

    Poly controller

---
+++ polycms-app-web/src/main/java/com/unidev/polycms/web/PolyController.java
@@ -0,0 +1,34 @@
+package com.unidev.polycms.web;
+
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Controller for poly records fetching
+ */
+@Controller
+public class PolyController {
+
+    @Autowired
+    private WebPolyCore webPolyCore;
+
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+
+    @RequestMapping("/poly/{polyId:.*}")
+    public String page(@PathVariable String polyId, Model model) {
+
+        WebPolyQuery polyQuery = WebPolyQuery.polyRequest()
+                .model(model)
+                .request(httpServletRequest)
+                .polyId(polyId)
+                ;
+        webPolyCore.addPoly(polyQuery);
+        model.addAttribute("view", "poly");
+        return "poly";
+    }
+}
