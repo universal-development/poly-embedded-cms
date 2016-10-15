c0399190646bc2c5724ec90315f5f6d82302d89e
Denis O <denis@universal-development.com>
2016-10-15 15:18:47 +0300

    Hateaos store index bean

---
+++ polycms-app-hateoas/src/main/java/com/unidev/polycms/hateoas/HateoasPolyIndex.java
@@ -0,0 +1,51 @@
+package com.unidev.polycms.hateoas;
+
+
+import org.springframework.hateoas.ResourceSupport;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HateoasPolyIndex extends ResourceSupport {
+
+    private HateoasPoly metadata;
+    private List<HateoasPoly> list;
+
+    public HateoasPolyIndex hateoasPolyIndex() {
+        return new HateoasPolyIndex();
+    }
+
+    public HateoasPolyIndex metadata(HateoasPoly metadata) {
+        this.metadata = metadata;
+        return this;
+    }
+
+    public HateoasPolyIndex list(List<HateoasPoly> list) {
+        this.list = list;
+        return this;
+    }
+
+    public HateoasPolyIndex add(HateoasPoly hateoasPoly) {
+        if (list == null) {
+            list = new ArrayList<>();
+        }
+        list.add(hateoasPoly);
+        return this;
+    }
+
+    public HateoasPoly getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(HateoasPoly metadata) {
+        this.metadata = metadata;
+    }
+
+    public List<HateoasPoly> getList() {
+        return list;
+    }
+
+    public void setList(List<HateoasPoly> list) {
+        this.list = list;
+    }
+}
