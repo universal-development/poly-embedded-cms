ace821d43a4f42271e226b33e6c256fecdd7a1b7
Denis O <denis@universal-development.com>
2016-10-15 17:31:48 +0300

    Pagination index handling

--- polycms-app-hateoas/src/main/java/com/unidev/polycms/hateoas/StoragePageController.java
+++ polycms-app-hateoas/src/main/java/com/unidev/polycms/hateoas/StoragePageController.java
@@ -1,28 +1,42 @@
 package com.unidev.polycms.hateoas;
 
 import com.unidev.polydata.domain.BasicPoly;
+import com.unidev.polyembeddedcms.PolyCore;
 import com.unidev.polyembeddedcms.PolyQuery;
+import com.unidev.polyembeddedcms.SQLitePolyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.LongStream;
 
 import static com.unidev.polycms.hateoas.HateoasPoly.hateoasPoly;
 import static com.unidev.polycms.hateoas.HateoasPolyIndex.hateoasPolyIndex;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
-@Controller
+@RestController
 public class StoragePageController {
 
+    @Autowired
+    private PolyCore polyCore;
+
+    @Autowired
+    private SQLitePolyService sqLitePolyService;
+
     @GetMapping(value = "/storage/{storage}/page/{page}", produces= MediaType.APPLICATION_JSON_VALUE)
-    public HateoasPolyIndex index(@PathVariable("storage") String storage, @PathVariable("page") Long page) {
-
+    public List<HateoasPoly> index(@PathVariable("storage") String storage, @PathVariable("page") Long page) {
         PolyQuery polyQuery = PolyQuery.query().page(page);
+        List<HateoasPoly> hateoasPolies = new ArrayList<>();
+        sqLitePolyService.listNewPoly(polyQuery, storage).forEach( poly -> {
+            hateoasPolies.add(hateoasPoly(poly));
+        });
 
-
-        return null;
+        return hateoasPolies;
     }
 
 }
