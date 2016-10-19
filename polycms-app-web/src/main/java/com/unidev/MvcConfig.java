99131fa577f5912179282a45937b54356e22fab6
Denis O <denis@universal-development.com>
2016-10-19 23:17:33 +0300

    Interceptor configuration replaced with controller advice

--- polycms-app-web/src/main/java/com/unidev/MvcConfig.java
+++ polycms-app-web/src/main/java/com/unidev/MvcConfig.java
@@ -1,25 +1,16 @@
 package com.unidev;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
-    @Autowired
-    private RequestInterceptor requestInterceptor;
-
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
     }
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(requestInterceptor);
-    }
 
 }
