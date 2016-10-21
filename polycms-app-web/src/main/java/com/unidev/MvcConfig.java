b8c9f7a0d3a54de0a46faf5f2f025663f9870309
Denis O <denis@universal-development.com>
2016-10-22 00:51:29 +0300

    Handling of /static/** as static paths

--- polycms-app-web/src/main/java/com/unidev/MvcConfig.java
+++ polycms-app-web/src/main/java/com/unidev/MvcConfig.java
@@ -1,6 +1,7 @@
 package com.unidev;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -12,5 +13,10 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
         registry.addViewController("/login").setViewName("login");
     }
 
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
 
 }
