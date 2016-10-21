641a46c44f75ccf374e873e892b3cf671b6a7e7d
Denis <denis@universal-development.com>
2016-10-21 22:35:39 +0300

    jsp pages moved in webapp

--- polycms-app-web/src/main/java/com/unidev/MvcConfig.java
+++ polycms-app-web/src/main/java/com/unidev/MvcConfig.java
@@ -1,11 +1,21 @@
 package com.unidev;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
