156cb569c87d276805b7598ffe5758c6bc47d289
Denis O <denis@universal-development.com>
2016-10-21 23:38:24 +0300

    jsp page path link

--- polycms-app-web/src/main/java/com/unidev/MvcConfig.java
+++ polycms-app-web/src/main/java/com/unidev/MvcConfig.java
@@ -10,6 +10,14 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
+    @Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
