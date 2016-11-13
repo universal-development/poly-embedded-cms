c5b204ae4b2254ebb043160ba8c54f5723d5adb5
Denis O <denis@universal-development.com>
2016-11-14 00:19:12 +0200

    Added constructors for exception

--- polycms-core/src/main/java/com/unidev/polyembeddedcms/PolyCoreException.java
+++ polycms-core/src/main/java/com/unidev/polyembeddedcms/PolyCoreException.java
@@ -6,4 +6,19 @@ import com.unidev.platform.common.exception.CommonRuntimeException;
  * Exception thrown on poly core error
  */
 public class PolyCoreException extends CommonRuntimeException {
+
+    public PolyCoreException() {
+    }
+
+    public PolyCoreException(String message) {
+        super(message);
+    }
+
+    public PolyCoreException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PolyCoreException(Throwable cause) {
+        super(cause);
+    }
 }
