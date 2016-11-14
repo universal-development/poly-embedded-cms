570a0cc9c615224d99071fecfd75f5d224fc2ee9
Denis O <denis@universal-development.com>
2016-11-14 22:23:25 +0200

    Imports cleanup

--- polycms-core/src/test/java/com/unidev/polyembeddedcms/PolyCoreDBTest.java
+++ polycms-core/src/test/java/com/unidev/polyembeddedcms/PolyCoreDBTest.java
@@ -6,7 +6,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
