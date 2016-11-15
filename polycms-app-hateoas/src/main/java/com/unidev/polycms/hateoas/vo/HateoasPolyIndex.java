b1aa837e70eb93bf5cf8f4095fa0dae9a3f98aea
Denis <denis@universal-development.com>
2016-11-15 17:26:19 +0200

    Index VO cleanup

--- polycms-app-hateoas/src/main/java/com/unidev/polycms/hateoas/vo/HateoasPolyIndex.java
+++ polycms-app-hateoas/src/main/java/com/unidev/polycms/hateoas/vo/HateoasPolyIndex.java
@@ -2,52 +2,12 @@ package com.unidev.polycms.hateoas.vo;
 
 
 import org.springframework.hateoas.ResourceSupport;
-import org.springframework.hateoas.mvc.ControllerLinkBuilder;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class HateoasPolyIndex extends ResourceSupport {
 
-    private HateoasPoly metadata;
-    private List<HateoasPoly> list;
 
     public static HateoasPolyIndex hateoasPolyIndex() {
         return new HateoasPolyIndex();
-    }
-
-    public HateoasPolyIndex metadata(HateoasPoly metadata) {
-        this.metadata = metadata;
-        return this;
-    }
-
-    public HateoasPolyIndex list(List<HateoasPoly> list) {
-        this.list = list;
-        return this;
-    }
-
-    public HateoasPolyIndex add(HateoasPoly hateoasPoly) {
-        if (list == null) {
-            list = new ArrayList<>();
-        }
-        list.add(hateoasPoly);
-        return this;
-    }
-
-    public HateoasPoly getMetadata() {
-        return metadata;
-    }
-
-    public void setMetadata(HateoasPoly metadata) {
-        this.metadata = metadata;
-    }
-
-    public List<HateoasPoly> getList() {
-        return list;
-    }
-
-    public void setList(List<HateoasPoly> list) {
-        this.list = list;
     }
 
 
