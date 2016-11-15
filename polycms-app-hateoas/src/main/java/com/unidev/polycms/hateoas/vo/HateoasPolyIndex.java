16a271255a18782a346b3d405067e7b20e2f9b17
Denis <denis@universal-development.com>
2016-11-15 17:42:08 +0200

    Added data field for poly index

--- polycms-app-hateoas/src/main/java/com/unidev/polycms/hateoas/vo/HateoasPolyIndex.java
+++ polycms-app-hateoas/src/main/java/com/unidev/polycms/hateoas/vo/HateoasPolyIndex.java
@@ -3,12 +3,25 @@ package com.unidev.polycms.hateoas.vo;
 
 import org.springframework.hateoas.ResourceSupport;
 
-public class HateoasPolyIndex extends ResourceSupport {
+public class HateoasPolyIndex<T> extends ResourceSupport {
 
+    T data;
 
     public static HateoasPolyIndex hateoasPolyIndex() {
         return new HateoasPolyIndex();
     }
 
 
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public HateoasPolyIndex data(T data) {
+        this.data = data;
+        return this;
+    }
 }
