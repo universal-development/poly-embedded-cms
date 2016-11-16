a774184b74a2a23d2c895540e64cd9d31f157767
Denis <denis@universal-development.com>
2016-11-16 16:50:52 +0200

    Disabled CSRF verifications

--- polycms-app-hateoas/src/main/java/com/unidev/ProjectnameApplication.java
+++ polycms-app-hateoas/src/main/java/com/unidev/ProjectnameApplication.java
@@ -61,6 +61,7 @@ public class ProjectnameApplication extends WebSecurityConfigurerAdapter impleme
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
 		http
 				.authorizeRequests()
 				.antMatchers("/jmx/**", "/logs").hasRole("ADMIN")
